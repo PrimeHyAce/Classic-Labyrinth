16fbb2a585b16a12ad6de7cd0e1c0b63b86d7a48
Faizeennn <muhammad1802ft.2020@student.uny.ac.id>
2023-05-22 11:30:01 +0700

    add timer and star

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -5,10 +5,40 @@ using TMPro;
 
 public class PlayManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text timerText;
+    [SerializeField] GameObject[] stars;
     [SerializeField] GameObject finishedCanvas;
     [SerializeField] TMP_Text finishedText;
-    int coin = 100;
 
+    [SerializeField] float timer;
+
+    public bool gameOn;
+
+    public void SetText(string text)
+    {
+        int Minutes = Mathf.FloorToInt(timer / 60);
+        int Seconds = Mathf.FloorToInt(timer % 60);
+        timerText.text = Minutes.ToString("00") + ":" + Seconds.ToString("00");
+    }
+
+    public void GameStart()
+    {
+        gameOn = true;
+    }
+
+    private void Update() {
+        if(gameOn)
+        {
+            timer -= Time.deltaTime;
+            SetText(timer.ToString());
+            if(timer <= 0)
+            {
+                gameOn = false;
+                GameOver();
+            }
+        }
+    }
+    
     public void GameOver()
     {
         finishedText.text = "Game Over";
@@ -17,12 +47,23 @@ public class PlayManager : MonoBehaviour
 
     public void PlayerWin()
     {
-        finishedText.text = "You Win!\nScore: " + GetScore() + "";
+        finishedText.text = "You Win!";
         finishedCanvas.SetActive(true);
-    }
 
-    private int GetScore()
-    {
-        return coin * 10;
+        if(timer > 90)
+        {
+            stars[0].SetActive(true);
+            stars[1].SetActive(true);
+            stars[2].SetActive(true);
+        }
+        else if(timer > 45)
+        {
+            stars[0].SetActive(true);
+            stars[1].SetActive(true);
+        }
+        else
+        {
+            stars[0].SetActive(true);
+        }
     }
 }
